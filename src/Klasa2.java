616027abd99b87a3f7f4cc8392380debebe35c58
Fuzzy183 <atila.veres@gmail.com>
2015-05-26 10:58:14 +0200

    class1+

---
+++ src/Klasa2.java
@@ -0,0 +1,19 @@
+
+public class Klasa2 {
+	private boolean logic;
+
+	public Klasa2(boolean logic) {
+		super();
+		this.logic = logic;
+	}
+
+	public boolean isLogic() {
+		return logic;
+	}
+
+	public void setLogic(boolean logic) {
+		this.logic = logic;
+	}
+	
+
+}
